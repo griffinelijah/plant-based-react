e83aa255be99686dd01b5e2da47ae2b7f7ad67b0
Griffin Delgado <griffinelijahd@gmail.com>
2019-11-20 16:13:39 -0600

    renders postContainer

---
+++ src/PostContainer/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class PostContainer extends Component {
+	constructor(){
+		super()
+		this.state = {
+
+		}
+	}
+	render(){
+		return (
+			<div>This is the PostContainer</div>
+		)
+	}
+}
+
+export default PostContainer
