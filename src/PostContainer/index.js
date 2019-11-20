0112671eb0384639657094335811e0fb381c240f
Griffin Delgado <griffinelijahd@gmail.com>
2019-11-20 17:10:22 -0600

    react app fetches list of posts from api

--- src/PostContainer/index.js
+++ src/PostContainer/index.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class PostContainer extends Component {
 	constructor(){
 		super()
 		this.state = {
+			posts: []
+		}
+	}
+	componentDidMount(){
+		this.getPosts();
+	}
 
+	getPosts = async () => {
+		try {//this will fetch all of the posts from our API
+			console.log("this is mu url >>> ", process.env.REACT_APP_API_URL + "/api/v1/posts/");
+			const posts = await fetch(process.env.REACT_APP_API_URL + '/api/v1/posts/');
+			//parsing the posts from the response 
+			console.log(posts);
+			const parsedPosts = await posts.json();
+			console.log(parsedPosts);
+		} catch(err) {
+			console.log(err);
 		}
 	}
 	render(){
